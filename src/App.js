607530cd87c91152256cde002a18ed7c0bf14e20
Jennifer Georgevich <jenngeorge@berkeley.edu>
2017-06-16 14:08:08 +0700

    Meetup request

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import $ from 'jquery';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {events: []}
+    this.searchEvents = this.searchEvents.bind(this);
+  }
+
+  componentDidMount(){
+    $.ajax({
+      method: 'GET',
+      dataType: 'jsonp',
+      url: `https://api.meetup.com/find/events?key=${process.env.REACT_APP_MEETUP_KEY}&text=tech`,
+    }).then(response => {
+        debugger
+        if (response.data){
+          this.setState({events: response.data})
+        }
+      })
+  }
+
+  searchEvents(){
+    debugger
+  }
+
   render() {
+    debugger
     return (
       <div className="App">
         <div className="App-header">
@@ -19,3 +44,15 @@ class App extends Component {
 }
 
 export default App;
+
+//
+// export const getEvents = (params) => {
+//     let requestUrl = 'https://api.meetup.com/find/events?sig_id=152336072&sig=bbdb62c39c10a55e0ed9442f3c335264e21e0cd5&';
+//     requestUrl += toQueryString(params);
+//     console.log(requestUrl)
+//     return $.ajax({
+//         type: 'GET',
+//         url: requestUrl,
+//         dataType: 'jsonp'
+//     });
+// };
